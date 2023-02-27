e190f60601924b3c9797aebb9786d9fb9dfced07
jhabermas <jhrc2@protonmail.com>
2023-02-27 01:50:27 +0000

    Add clang-tidy to cmake

--- src/hello/hello.cpp
+++ src/hello/hello.cpp
@@ -1,7 +1,13 @@
 #include "hello/hello.h"
 #include <fmt/format.h>
 
+#include <iostream>
+
 std::string hello::say_hello(std::string_view name)
 {
+    std::string a = "asdf";
+    std::string b = std::move(a);
+    std::cout << a << b;
+
     return fmt::format("Hello {}", name);
 }
